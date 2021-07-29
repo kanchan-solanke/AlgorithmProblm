802fa393b525546b762c7a0d6f0593dd756be0fd
kanchan-solanke <kanchanssolanke@gmail.com>
2021-07-29 06:10:03 +0530

    to find prime numbers and after extends to check it is palindrome or not

--- AlgorithmMain.java
+++ AlgorithmMain.java
@@ -1,72 +1,75 @@
 import java.util.Arrays;
 import java.util.Scanner;
-
-public class Anagram {
-    public static void main(String args[]) {
-
-
+public class prime_numbers_in_a_given_range
+{
+    public static void main(String[] args)
+    {
         //scanner class object creation
-        Scanner sc = new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
         //input from user
-        System.out.println("Enter Starting Number : ");
+        System.out.print("Enter Starting Number : ");
         int start = 0;
         System.out.print("Enter Ending Number : ");
         int end = 1000;
-        System.out.println("Prime numbers between " + start + " and " + end + " are : ");
+        System.out.println("Prime numbers between "+start+" and "+end+" are : ");
         int count = 0;
         //loop for finding and printing all prime numbers between given range
-        int i;
-        for (i = start; i <= end; i++) {
+        for(int i = start ; i <= end ; i++) {
             //logic for checking number is prime or not
             count = 0;
             for (int j = 1; j <= i; j++) {
                 if (i % j == 0)
                     count = count + 1;
             }
-            String a = " ";
             if (count == 2)
-                 System.out.println(i);
+                System.out.println(i);
+
         }
+            // check palindromes
 
+            //declare and initialize variables
+            int  temp,i;
+            int sum = 0;
 
+            System.out.println("Enter number to check for palindrome");
+            //get input from user
+            i = sc.nextInt();
 
-//    /* function to check whether two strings are
-//    anagram of each other */
-//        static boolean areAnagram ( char[] str1, char[] str2){
-//
-//// Get lengths of both strings
-//            int n1 = str1.length;
-//            int n2 = str2.length;
-//
-//            // If length of both strings is not same,
-//            // then they cannot be anagram
-//            if (n1 != n2)
-//                return false;
-//
-//            // Sort both strings
-//            Arrays.sort(str1);
-//            Arrays.sort(str2);
-//
-//            // Compare sorted strings
-//            for (int i = 0; i < n1; i++) {
-//                if (str1[i] != str2[i]) {
-//                    return false;
-//                }
-//            }
-//            return true;
-//        }
-//
-//        /* Driver Code*/
-//        public static void main (String args[]){
-//            char str1[] = {'a', 'b', 'c', 'd'};
-//            char str2[] = {'d', 'c', 'b', 'a'};
-//
-//            // Function Call
-//            if (areAnagram(str1, str2))
-//                System.out.println("The two strings are anagram of each other");
-//            else
-//                System.out.println("The two strings are not anagram of each other");
-//        }
+            //store number in a temporary variable temp
+            temp = i;
 
+            //use for loop check whether number is prime or not
+            for(i = 1; i <= temp; i++)
+            {
+                if(temp%i == 0)
+                {
+                    count++;    //increment counter when the reminder is 0
+                }
+            }
+
+            //use while loop to check whether the number is palindrome or not
+            while(i > 0)
+            {
+                // get last digit of the number
+                int rem = i % 10;
+
+                // store the digit last digit
+                sum = sum*10+rem;
+
+                // extract all digit except the last
+                i = i/10;
+            }
+
+            //check whether the number is palindrome and Prime or not
+            if(temp == sum && count == 2)
+            {
+                System.out.println(temp +" is a PalPrime number");
+            }
+            else
+            {
+                System.out.println(temp +" is not a PalPrime number");
+            }
+
+        }
     }
-}
+
