ffab25fcaec568d4df0e7bc118a2f459f7fc6a87
kanchan-solanke <kanchanssolanke@gmail.com>
2021-07-29 06:06:45 +0530

    The Two Strings are Anagram or not....

--- AlgorithmMain.java
+++ AlgorithmMain.java
@@ -1,36 +1,72 @@
-public class BubbleSort{
-    // method for bubble sort
-    static void bubbleSort(int[] arr) {
-        int n = arr.length;
-        int temp = 0;
-        for(int i=0; i < n; i++){
-            for(int j=1; j < (n-i); j++){
-                if(arr[j-1] > arr[j]){
-                    //swap elements
-                    temp = arr[j-1];
-                    arr[j-1] = arr[j];
-                    arr[j] = temp;
-                }
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Anagram {
+    public static void main(String args[]) {
+
+
+        //scanner class object creation
+        Scanner sc = new Scanner(System.in);
+        //input from user
+        System.out.println("Enter Starting Number : ");
+        int start = 0;
+        System.out.print("Enter Ending Number : ");
+        int end = 1000;
+        System.out.println("Prime numbers between " + start + " and " + end + " are : ");
+        int count = 0;
+        //loop for finding and printing all prime numbers between given range
+        int i;
+        for (i = start; i <= end; i++) {
+            //logic for checking number is prime or not
+            count = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0)
+                    count = count + 1;
             }
+            String a = " ";
+            if (count == 2)
+                 System.out.println(i);
         }
 
-    }
-    public static void main(String[] args) {
-        int arr[] ={10,456,89,32,46,890};
 
-        System.out.println("Array Before Bubble Sort");
-        for(int i=0; i < arr.length; i++){
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
 
-        bubbleSort(arr);//sorting array elements using bubble sort
-
-        System.out.println("Array After Bubble Sort");
-        for(int i=0; i < arr.length; i++){
-            System.out.print(arr[i] + " ");
-        }
+//    /* function to check whether two strings are
+//    anagram of each other */
+//        static boolean areAnagram ( char[] str1, char[] str2){
+//
+//// Get lengths of both strings
+//            int n1 = str1.length;
+//            int n2 = str2.length;
+//
+//            // If length of both strings is not same,
+//            // then they cannot be anagram
+//            if (n1 != n2)
+//                return false;
+//
+//            // Sort both strings
+//            Arrays.sort(str1);
+//            Arrays.sort(str2);
+//
+//            // Compare sorted strings
+//            for (int i = 0; i < n1; i++) {
+//                if (str1[i] != str2[i]) {
+//                    return false;
+//                }
+//            }
+//            return true;
+//        }
+//
+//        /* Driver Code*/
+//        public static void main (String args[]){
+//            char str1[] = {'a', 'b', 'c', 'd'};
+//            char str2[] = {'d', 'c', 'b', 'a'};
+//
+//            // Function Call
+//            if (areAnagram(str1, str2))
+//                System.out.println("The two strings are anagram of each other");
+//            else
+//                System.out.println("The two strings are not anagram of each other");
+//        }
 
     }
 }
